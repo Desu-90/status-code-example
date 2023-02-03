af8ccda8e2e64166c7ed4f580d942d3312a7339b
Dennis Olmedo (RIT Student) <dao9631@rit.edu>
2023-02-03 15:55:24 -0500

    In class work, not much going on

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const url = require('url');
 const query = require('querystring');
 const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
+const { parse } = require('path');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -16,12 +17,19 @@ const urlStruct = {
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
   
+  const parsedQuery = query.parse(parsedUrl.query);
+
+  const status = parsedQuery.status;
+  const admin  = parsedQuery.admin;
+  const name = parsedQuery.name;
+  
+  debugger;
 
   const func = urlStruct[parsedUrl.pathname];
   if (func) {
-    func(request, response, params);
+    func(request, response, parsedQuery); //params);
   } else {
-    urlStruct.notFound(request, response, params);
+    urlStruct.notFound(request, response, parsedQuery);//params);
   }
 
 };
